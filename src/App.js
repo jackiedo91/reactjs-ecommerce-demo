ca15e20a28099552009542241faeb0d4028e61db
Jackie <huydo91@outlook.com>
2020-01-05 09:49:26 +0700

    modulize code into components and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 // Custom components
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// Components
+import Directory from '../../components/directory/directory.component';
+
+// Styling
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+
+export default HomePage;
