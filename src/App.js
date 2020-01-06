6898d29aaf20f636d9fec41dfd39d521b1ee5618
Jackie <huydo91@outlook.com>
2020-01-06 20:12:37 +0700

    add header component to existing pages

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 // Custom components
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ HomePage }/>
         <Route exact path='/shop' component={ ShopPage }/>
