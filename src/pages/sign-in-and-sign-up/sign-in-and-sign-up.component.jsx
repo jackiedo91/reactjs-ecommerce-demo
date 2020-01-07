956410a78e0b6ec209926145a2e5257bd85203a2
Jackie <huydo91@outlook.com>
2020-01-07 08:21:46 +0700

    create sign in with custom form input

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// Styling
+import './sign-in-and-sign-up.styles.scss';
+// Custom Components
+import SignIn from '../../components/sign-in/sign-in.component';
+
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+)
+
+export default SignInAndSignUpPage;
