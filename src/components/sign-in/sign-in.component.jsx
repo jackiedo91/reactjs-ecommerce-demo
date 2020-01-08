24ae4238b0b30ff46c04b14c47f250debfa82678
Jackie <huydo91@outlook.com>
2020-01-08 14:09:34 +0700

    finish sign in and sign up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // ThirdParty
-import { signInWithGoogle } from '../../firebase/firebase.utls';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utls';
 
 // Styling
 import './sign-in.styles.scss';
@@ -19,13 +19,17 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
-    event.preventDefaut();
+  handleSubmit = async event => {
+    event.preventDefault();
 
-    this.setState({
-      email: '',
-      password: ''
-    })
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   handleChange = event => {
