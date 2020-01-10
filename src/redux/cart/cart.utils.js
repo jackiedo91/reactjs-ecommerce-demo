d9e8968854c682dfc22ade341962cf612709f6e3
Jackie <huydo91@outlook.com>
2020-01-10 10:50:22 +0700

    complete checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, {...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  // remove cart item completely from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem =>
+      cartItem.id !== cartItemToRemove.id
+    )
+  }
+  // Descrease quantity of matched item
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
+}
