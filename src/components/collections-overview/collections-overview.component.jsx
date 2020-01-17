0f18d34c80c7c5f503da9d0d51beddee26e65d29
Jackie <huydo91@outlook.com>
2020-01-17 17:02:49 +0700

    data normalization for shop data

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -9,7 +9,7 @@ import './collections-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 // Redux actions
-import { selectCollections } from '../../redux/shop/shop.selector'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector'
 
 
 const CollectionsOverview = ({ collections }) => (
@@ -23,7 +23,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
